d535a79fd7462f26cfa8237fdada7535a02d8862
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-04-30 08:21:09 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,19 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
 };
 
 export default nextConfig;
-
