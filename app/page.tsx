f6d22664ade9fd8bc7a70630f30d0134cbebdb63
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-04-30 18:29:11 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const HERO_SLIDES = [
    "https://res.cloudinary.com/dx3k7hbnc/image/upload/v1777562618/hero-1_jlcvld.jpg",
   "https://res.cloudinary.com/dx3k7hbnc/image/upload/v1777569332/hero-2_w8amfo.jpg",
   "https://res.cloudinary.com/dx3k7hbnc/image/upload/v1777569685/hero-3_oqrukn.jpg",
-  "https://images.unsplash.com/photo-1566073771259-6a8506099945?auto=format&fit=crop&w=1920&q=85",
+  "https://res.cloudinary.com/dx3k7hbnc/image/upload/v1777570096/hero-4_d2xzfi.jpg",
   "https://images.unsplash.com/photo-1611892440504-42a792e24d32?auto=format&fit=crop&w=1920&q=85",
 ];
 
