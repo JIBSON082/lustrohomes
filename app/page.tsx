978f887f67296428ba36b9d3cb90a06c38af5499
JIBSON082 <105949776+JIBSON082@users.noreply.github.com>
2026-05-11 16:01:04 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -635,7 +635,7 @@ function Hero() {
       {/* ── Hero Video Block ── */}
     <div
   className="hero-video-block w-full bg-charcoal relative overflow-hidden"
-  style={{ opacity: 0, height: "42vh", marginTop: "6px" }}
+  style={{ opacity: 0, height: "40vh", marginTop: "2px" }}
 >
       
         <video
